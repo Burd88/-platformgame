322e06e6cddb6bc858335ed5c2f34e8bf436fb53
Burd88 <32732780+Burd88@users.noreply.github.com>
2019-05-28 17:30:49 +0700

    отчистка еще

--- player/Player.gd
+++ player/Player.gd
@@ -59,48 +59,15 @@ func _move(delta):
 		
 	elif direction.x == 0 and direction.y == 0 and swim == false and open_door == false and attack == false and !is_on_wall():
 		if velocity.y == 0:
-	
 			$spr.animation = "idle"
-			
 		elif velocity.y > 0:
 			$spr.animation = "fall"
 
 	if direction.x > 0:
 		$spr.flip_h = false
-	#	$colis_down.position.x = 4
-	#	$colis_up.position.x = 4
-		#$check_cell.position.x = 4
-		#$colis_hang.position.x = 10
-		#$colis_hang.position.y = -12
-		#$colis_hang.rotation_degrees = 270
+
 	elif direction.x < 0:
 		$spr.flip_h = true
-	#	$colis_down.position.x = -4
-		#$colis_up.position.x = -4
-		#$check_cell.position.x = -4
-		#$colis_hang.position.x = -10
-		#$colis_hang.position.y = -12
-		#$colis_hang.rotation_degrees = 90
-		
-	#if direction.y == -1 :#and check_cell:
-	#	$colis_up.disabled = true
-	#	$check_cell.enabled = true
-		#$check_cell.show()
-	#elif direction.y == 1 :#and !check_cell:
-	#	$colis_up.disabled = false 
-	#	$check_cell.enabled = false
-		#$check_cell.hide()
-	#else : 
-	#	$colis_up.disabled = false 
-		#$check_cell.enabled = false
-		#$check_cell.hide()
-		
-	#if $check_cell.is_colliding():
-		#check_cell = true
-		#print($check_cell.get_collider())
-		#print($check_cell.get_collision_normal()) 
-	#else:
-		#check_cell = false
 	
 	distance.x = speed*delta
 	velocity.x = (direction.x*distance.x)/delta
@@ -133,12 +100,12 @@ func _move(delta):
 		$spr.animation = "hang1"
 		
 		
-		#if Input.is_action_just_pressed("ui_up"):
-		#	velocity.y = -jump_speed
-		#	direction.y = 1
-		#if Input.is_action_just_pressed("ui_down"):
-		#	velocity.y = jump_speed
-		#	direction.y = 1
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = -jump_speed
+			direction.y = 1
+		if Input.is_action_just_pressed("ui_down"):
+			velocity.y = jump_speed
+			direction.y = 1
 		
 	if is_on_ceiling():
 		velocity.y = 0
