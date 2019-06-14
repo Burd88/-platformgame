69020386a3e40b511e8f925913868cdc421862fb
Burd88 <32732780+Burd88@users.noreply.github.com>
2019-06-14 18:58:02 +0700

    фикс перевода текста

--- translation/translationt.gd
+++ translation/translationt.gd
@@ -6,12 +6,12 @@ var tr_text
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+
+	pass # Replace with function body.
+
+func _process(delta):
 	if language == 1:
 		tr_text = 'Тут очень темно.... и сыро... \n Тебе нужно взять факел'
 	elif language == 2:
 		tr_text = 'this dark... \n you need torch'
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
 #	pass
