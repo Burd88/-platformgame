e0afeff96ec815d0af92420291a83f90c2f845e0
Burd88 <32732780+Burd88@users.noreply.github.com>
2019-07-18 14:05:48 +0700

    добавил анимацию воды которая льется на шестерни

--- levels/Level1/Level1.gd
+++ levels/Level1/Level1.gd
@@ -4,6 +4,7 @@ var tourch = false
 var lever1 = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$CanvasModulate.show()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
