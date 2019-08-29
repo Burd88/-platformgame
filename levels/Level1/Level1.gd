2a4d362951511037a77726c570e9c7d09d99ecc9
Burd88 <32732780+Burd88@users.noreply.github.com>
2019-08-29 18:42:50 +0700

    ковыряю загрузку игры

--- levels/Level1/Level1.gd
+++ levels/Level1/Level1.gd
@@ -168,10 +168,11 @@ func _on_save_pressed():
 
 
 func _on_Button4_pressed():
-	print("должно быть загружено сохранение")
+	
 	var save_game = File.new()
 	if not save_game.file_exists("res://savegame.save"):
-		return # Error! We don't have a save to load.
+		
+		return print("error")# Error! We don't have a save to load.
 
     # We need to revert the game state so we're not cloning objects
     # during loading. This will vary wildly depending on the needs of a
@@ -180,19 +181,23 @@ func _on_Button4_pressed():
 	var save_nodes = get_tree().get_nodes_in_group("save_group")
 	for i in save_nodes:
 		i.queue_free()
-
+	
     # Load the file line by line and process that dictionary to restore
     # the object it represents.
 	save_game.open("res://savegame.save", File.READ)
 	while (!save_game.eof_reached()):
+		
 		var current_line = parse_json(save_game.get_line())
+		
         # Firstly, we need to create the object and add it to the tree and set its position.
-
-		var new_object = load(current_line["filename"]).instance()
+		
+		var new_object = load("Enemy/Bat/Bat.tscn").instance()
+		
 		get_node(current_line["parent"]).add_child(new_object)
+		
 		new_object.position = Vector2(current_line["pos_x"], current_line["pos_y"])
         # Now we set the remaining variables.
-		print(current_line.keys())
+		
 		for i in current_line.keys():
 			if i == "filename" or i == "parent" or i == "pos_x" or i == "pos_y":
 				continue
