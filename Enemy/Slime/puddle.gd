5b7e25ef08b6d57cf2564c984544fffe9e95d819
Burd88 <32732780+Burd88@users.noreply.github.com>
2019-09-03 19:02:38 +0700

    чета сделал

--- Enemy/Slime/puddle.gd
+++ Enemy/Slime/puddle.gd
@@ -7,49 +7,74 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.wait_time = 60
-	print("лужа")
+	
 	
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if $ray1.is_colliding() :
+
+	
+	if $ray1.is_colliding() == true:
 		print(1)
 		rotation_degrees = 90
+		#while $RayCast2D.is_colliding() == false:
+		#	position.y +=10
+		#while $RayCast2D2.is_colliding() == false:
+		#	position.y -=10
 		$ray1.enabled = false
 		$ray2.enabled = false
-	elif $ray2.is_colliding() :
+	elif $ray2.is_colliding() == true :
 		rotation_degrees = 270
 		print(2)
+		#while $RayCast2D2.is_colliding() == false:
+		#	position.y +=10
+		#while $RayCast2D.is_colliding() == false:
+		#	position.y -=10
 		$ray1.enabled = false
 		$ray2.enabled = false
-	else:
-		print(3)
-		$ray1.enabled = false
-		$ray2.enabled = false
+	#else:
 		
-	if rotation_degrees== 90 and  $RayCast2D.is_colliding() == false:
-		position.y += 10
-		print(4)
-		$RayCast2D.enabled = false
-	elif rotation_degrees== 270 and $RayCast2D.is_colliding() == false:
-		position.y -= 10
-		print(5)
-		$RayCast2D2.enabled = false
-	else : 
-		print(6)
-		pass
+		#$ray1.enabled = false
+		#$ray2.enabled = false
 		
-	if rotation_degrees== 90 and  $RayCast2D2.is_colliding() == false:
-		position.y -= 10
-		print(7)
-		$RayCast2D.enabled = false
-	elif rotation_degrees== 270 and  $RayCast2D2.is_colliding() == false:
-		position.y += 10
-		print(8)
-		$RayCast2D2.enabled = false
-	else : 
-		print(9)
+	#if rotation_degrees == 90 :
+	#	if $RayCast2D.is_colliding() == false:
+	#		position.y += 10
+	#		$RayCast2D.enabled = false
+	#	elif $RayCast2D2.is_colliding() == false:
+	#		position.y -= 10
+	#		$RayCast2D2.enabled = false
+	#	else:
+	#		pass
+	#elif rotation_degrees == 270:
+	#	if $RayCast2D.is_colliding() == false:
+	#		position.y -= 10
+	#		$RayCast2D.enabled = false
+	#	elif $RayCast2D2.is_colliding() == false:
+	#		position.y += 10
+	#		$RayCast2D2.enabled = false
+	#	else: pass
+		#print(4)
+		#$RayCast2D.enabled = false
+	#elif rotation_degrees== 270 and $RayCast2D.is_colliding() == false:
+		#position.y -= 10
+		#print(5)
+		#$RayCast2D2.enabled = false
+	#else : 
+		
+	#	pass
+		
+	#if rotation_degrees== 90 and  $RayCast2D2.is_colliding() == false:
+		#position.y -= 10
+	#	print(7)
+		#$RayCast2D.enabled = false
+	#elif rotation_degrees== 270 and  $RayCast2D2.is_colliding() == false:
+		#position.y += 10
+	#	print(8)
+		#$RayCast2D2.enabled = false
+	#else : 
+		
 		pass
 	#print($Timer.time_left)
 	scale -= Vector2(0.001, 0.001)
@@ -66,15 +91,13 @@ func _on_Timer_timeout():
 	print("delete timer")
 	pass # Replace with function body.
 
-
-func _on__body_entered(body):
-	if body.name == "Player":
-		body.health_now -= 100
-		print("damage")
-	pass # Replace with function body.
-
-
 func _on_Timer2_timeout():
 	$CollisionShape2D.disabled = true
 	$CollisionShape2D.disabled = false
 	pass # Replace with function body.
+
+func _on_puddle_body_entered(body):
+	if body.name == "Player":
+		body.health_now -= 100
+		print("damage")
+	pass
